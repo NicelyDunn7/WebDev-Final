ce6bc779870af00d8a1f9084399f355adce076a1
dpdunn10 <dpdunn10@gmail.com>
2016-01-26 20:53:11 -0600

    Create home.php

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Delta Sigma Phi - Beta Beta</title>
+		<link rel="icon" type="image/png" href="images/crest.png">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="jquery-ui-1.11.4.custom/jquery-ui.min.css" type="text/css">
+		<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css">
+		<script src="jquery-ui-1.11.4.custom/external/jquery/jquery.js"></script>
+		<script src="../jquery-2.1.3.min.js"></script>
+		<script src="jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>		
+	</head>
+	<body>
+		<?php include("includes/header.html");?>
+		<?php include("includes/navbar.html");?>
+		<div id="content">
+			<?php include("pages/welcome.html");?>
+		</div>
+		<?php include("includes/footer.html");?>
+	</body>
+</html>
